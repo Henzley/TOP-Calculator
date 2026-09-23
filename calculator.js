b8b180dd7e71807a81d4467e52490d75c64e8f42
Henzley Spogter <spoggie.hs@gmail.com>
2026-09-23 12:36:30 +0200

    Create variables for each part of the operation.
    To be used to update the display.

--- calculator.js
+++ calculator.js
@@ -13,3 +13,9 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+//Create variables for each part of the operation.
+//Used to update the display.
+let num1;
+let num2;
+let operator;
